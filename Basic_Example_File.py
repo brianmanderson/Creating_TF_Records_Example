2246b5c4f09abd1402a0549c482664792074fdfc
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-09-16 15:07:49 -0700

    update ignore file, add basic file write and read

---
+++ Basic_Example_File.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+from Data_Generators.Image_Processors_Module.src.Processors.TFRecordWriter import dictionary_to_tf_record
+
+
+example = {
+    'Patient_Image': np.random.random((10, 512, 512)),
+    'Patient_Name': 'John Doe',
+    'Patient_Age': 53,
+    'Patient_Weight': 123.4,
+    'Patient_Location': 'Houston/San Diego'
+}
+
+dictionary_to_tf_record(os.path.join('.', 'TFRecords', 'Basic',
+                                     'base_example.tfrecord'),
+                        example)
+
+example = {
+    'Patient_Image': np.random.random((5, 128, 128)),
+    'Patient_Name': 'Jane Doe',
+    'Patient_Weight': 83.4,
+    'Patient_Age': 34,
+    'Patient_Location': 'LA'
+}
+
+dictionary_to_tf_record(os.path.join('.', 'TFRecords', 'Basic',
+                                     'base_example2.tfrecord'),
+                        example)
---
+++ Reading_Basic_Example.py
@@ -0,0 +1,17 @@
+from Data_Generators.TFRecord_to_Dataset_Generator import DataGeneratorClass
+import Data_Generators.Image_Processors_Module.src.Processors.TFDataSetProcessors as Processors
+
+train_paths = [r'C:\Users\b5anderson\Desktop\Modular_Projects\TeachingTFRecords\TFRecords\Basic']
+
+data_generator = DataGeneratorClass(record_paths=train_paths)
+
+data_iterator = iter(data_generator.data_set)
+data = next(data_iterator)
+data2 = next(data_iterator)
+xxx = 1
+processors = [
+    Processors.ExpandDimension(axis=-1, image_keys=('image_array', 'annotation_array'),),
+    Processors.ReturnOutputs(input_keys=('image_array',), output_keys=('annotation_array',)),
+    {'batch': 1},
+    {'repeat'}
+]
