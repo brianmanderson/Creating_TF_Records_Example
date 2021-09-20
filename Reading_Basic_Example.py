37562d4b2c899999cde90609f4a80fd11519550d
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-09-20 11:57:05 -0700

    remove spaces

--- Reading_Basic_Example.py
+++ Reading_Basic_Example.py
@@ -2,9 +2,7 @@ from Data_Generators.TFRecord_to_Dataset_Generator import DataGeneratorClass
 import Data_Generators.Image_Processors_Module.src.Processors.TFDataSetProcessors as Processors
 
 train_paths = [r'C:\Users\b5anderson\Desktop\Modular_Projects\TeachingTFRecords\TFRecords\Basic']
-
 data_generator = DataGeneratorClass(record_paths=train_paths)
-
 data_iterator = iter(data_generator.data_set)
 data = next(data_iterator)
 data2 = next(data_iterator)
