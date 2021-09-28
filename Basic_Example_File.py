b04d7505520848d800c845cc7e47d9f2205bba65
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-09-28 09:55:54 -0700

    update submodules, expand example

--- Basic_Example_File.py
+++ Basic_Example_File.py
@@ -5,6 +5,7 @@ from Data_Generators.Image_Processors_Module.src.Processors.TFRecordWriter impor
 
 example = {
     'Patient_Image': np.random.random((10, 512, 512)),
+    'Patient_Mask': np.random.random((10, 512, 512)).astype('int'),
     'Patient_Name': 'John Doe',
     'Patient_Age': 53,
     'Patient_Weight': 123.4,
@@ -17,6 +18,7 @@ dictionary_to_tf_record(os.path.join('.', 'TFRecords', 'Basic',
 
 example = {
     'Patient_Image': np.random.random((5, 128, 128)),
+    'Patient_Mask': np.random.random((5, 128, 128)).astype('int'),
     'Patient_Name': 'Jane Doe',
     'Patient_Weight': 83.4,
     'Patient_Age': 34,
--- Reading_Basic_Example.py
+++ Reading_Basic_Example.py
@@ -1,15 +1,17 @@
 from Data_Generators.TFRecord_to_Dataset_Generator import DataGeneratorClass
 import Data_Generators.Image_Processors_Module.src.Processors.TFDataSetProcessors as Processors
+from PlotScrollNumpyArrays.Plot_Scroll_Images import plot_scroll_Image
 
 train_paths = [r'C:\Users\b5anderson\Desktop\Modular_Projects\TeachingTFRecords\TFRecords\Basic']
 data_generator = DataGeneratorClass(record_paths=train_paths)
+processors = [
+    Processors.ExpandDimension(axis=-1, image_keys=('Patient_Image', 'Patient_Mask'),),
+    Processors.ReturnOutputs(input_keys=('Patient_Image',), output_keys=('Patient_Mask',)),
+    # {'repeat'}
+]
+data_generator.compile_data_set(image_processors=processors, debug=True)
 data_iterator = iter(data_generator.data_set)
 data = next(data_iterator)
 data2 = next(data_iterator)
 xxx = 1
-processors = [
-    Processors.ExpandDimension(axis=-1, image_keys=('image_array', 'annotation_array'),),
-    Processors.ReturnOutputs(input_keys=('image_array',), output_keys=('annotation_array',)),
-    {'batch': 1},
-    {'repeat'}
-]
+
